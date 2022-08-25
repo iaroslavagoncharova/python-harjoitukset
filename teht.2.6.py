e857c90b483811839f332c8c1c0171336de2d470
darri <iaroslag@metropolia.fi>
2022-08-25 12:34:25 +0300

    .

--- teht.2.6.py
+++ teht.2.6.py
@@ -3,10 +3,10 @@ import random
 x1 = random.randint(0,9)
 x2 = random.randint(0,9)
 x3 = random.randint(0,9)
-print(x1, x2, x3)
+print(x1, x2, x3, sep='')
 
 y1 = random.randint (1,6)
 y2 = random.randint (1,6)
 y3 = random.randint (1,6)
 y4 = random.randint (1,6)
-print(y1, y2, y3, y4)
+print(y1, y2, y3, y4, sep='')
