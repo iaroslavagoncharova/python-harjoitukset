f4d9aadf9b0fec780643a94bcd3d9fbbd6a4bf4a
Iaroslava Goncharova <111697910+iaroslavagoncharova@users.noreply.github.com>
2022-08-25 13:40:30 +0300

    Update teht. 2.2.py

--- teht. 2.2.py
+++ teht. 2.2.py
@@ -1,4 +1,4 @@
 import math
-r = (float)(input('Anna ympyrän säde: '))
-C = math.pi*math.pow(r, 2)
+säde = (float)(input('Anna ympyrän säde: '))
+C = math.pi*math.pow(säde, 2)
 print(f'Ympyrän pinta-ala on {C:.3f}')
