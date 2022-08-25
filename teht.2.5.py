6a6cb9752c51baa9ca3c843b7dc8c95d5f9d4fb8
darri <iaroslag@metropolia.fi>
2022-08-25 12:36:19 +0300

    .

--- teht.2.5.py
+++ teht.2.5.py
@@ -4,7 +4,7 @@ na = (float) (input ('Anna naulat: '))
 lu = (float) (input ('Anna luodit: '))
 grammat = 20 * 32 * 13.3 * le + 32 * 13.3 * na + 13.3 * lu
 kilot = math.floor(grammat/1000)
-grammat = grammat - kilot*1000
-print(f'Massa nykymittojen mukaan:\n {kilot} kilogrammaa ja {grammat:.2f} grammaa.')
-# en pystynyt tulostamaan kiloja ja grammoja samaan aikaan
+grammat2 = grammat - kilot*1000
+print(f'Massa nykymittojen mukaan:\n {kilot} kilogrammaa ja {grammat2:.2f} grammaa.')
+
 
