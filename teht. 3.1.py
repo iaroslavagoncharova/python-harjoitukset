c91f2e48987d2a3c70a9e7867b8e99f5606826d9
darri <iaroslag@metropolia.fi>
2022-08-29 15:23:00 +0300

    .

---
+++ teht. 3.1.py
@@ -0,0 +1,6 @@
+kuhan_pituus = int(input('Anna kuhan pituus'))
+if kuhan_pituus < 37:
+    puuttuu_senttiä = (kuhan_pituus - 37) * -1
+    print(f'Puttuu {puuttuu_senttiä} cm. Laske kuha takaisin järveen.')
+
+
---
+++ teht. 3.3.py
@@ -0,0 +1,27 @@
+# Miehen normaali hemoglobiiniarvo on välillä 134-195 g/l.
+# Naisen normaali hemoglobiiniarvo on välillä 117-175 g/l.
+gender = input('Sukupuolesi (nainen/mies)?')
+hg_value = int(input('Hemoglobiinisi (g/l)?'))
+
+if gender == 'nainen':
+    #testataan naisen ohjearvot
+    if not (5 < hg_value < 300):
+        print('Virheellinen hg-arvo')
+    if hg_value < 117:
+        print('Hemoglobiiniarvo on alhainen')
+    elif hg_value < 175:
+        print('Hemoglobiiniarvo on normaali')
+    else:
+        print('Hemoglobiiniarvo on korkea')
+elif gender == 'mies':
+    #testataan miehen arvot
+    if not (5 < hg_value < 300):
+        print('Virheellinen hg-arvo')
+    if hg_value < 134:
+        print('Hemoglobiiniarvo on alhainen')
+    elif hg_value < 195:
+        print('Hemoglobiiniarvo on normaali')
+    else:
+        print('Hemoglobiiniarvo on korkea')
+
+
