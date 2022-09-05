8b1fb550f5e87d8a91ad25890817d3508d173a18
darri <iaroslag@metropolia.fi>
2022-09-05 10:52:25 +0300

    .

--- teht.4.6.py
+++ teht.4.6.py
@@ -1,1 +1,14 @@
+import random
+inside = 0
+i = 0
+n = int(input('Anna pisteiden määrä:'))
 
+while i< 0:
+    x = random.uniform (-1,1)
+    y = random.uniform (-1,1)
+    if x**2 + y**2 <=1:
+        inside +=1
+    i+=1
+
+pi = 4 * inside / n
+print(f'pi on {pi}')
