a4e5b1ffcbc6bb33987eae1f7f48ee2823689b04
darri <iaroslag@metropolia.fi>
2022-09-07 15:45:48 +0300

    .

---
+++ teht.6.4.py
@@ -0,0 +1,14 @@
+#Kirjoita funktio, joka saa parametrinaan listan kokonaislukuja.
+#Ohjelma palauttaa listassa olevien lukujen summan.
+#Kirjoita testausta varten pääohjelma, jossa luot listan, kutsut funktiota ja tulostat sen palauttaman summan.
+userInput = int(input('Anna luvut: ')
+
+
+summa = 0
+luku = int(input('Anna luku1: ')
+for n in lista:
+    luku = int(input('Anna luku1: ')
+    lista.append(luku)
+    summa += luku
+
+    print(summa)
