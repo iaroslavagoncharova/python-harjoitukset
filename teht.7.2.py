c82ee956f6bfae69cd1d7b621a8b5632e553de81
darri <iaroslag@metropolia.fi>
2022-09-12 12:58:50 +0300

    .

--- teht.7.2.py
+++ teht.7.2.py
@@ -4,3 +4,23 @@
 #Lopuksi ohjelma luettelee syötetyt nimet yksi kerrallaan allekkain mielivaltaisessa järjestyksessä.
 #Käytä joukkotietorakennetta nimien tallentamiseen.
 nimet = set()
+nimi = input('Anna nimi: ')
+while nimi != ' ':
+    if nimi in nimet:
+        print('Aiemmin syötetty nimi')
+        nimi = input('Anna nimi: ')
+
+    elif nimi not in nimet:
+        print('Uusi nimi')
+        nimet.add(nimi)
+        nimi = input('Anna nimi: ')
+
+    else:
+        break
+
+for n in nimet:
+    print(n)
+
+
+
+
