cdd40e8f2512d405c81dd2ec3090cae06bb08aa3
darri <iaroslag@metropolia.fi>
2022-09-12 12:40:21 +0300

    .

--- teht.7.1.py
+++ teht.7.1.py
@@ -1,14 +1,15 @@
 vuodenajat = ("talvi", "kevät", "kesä", "syksy")
 kuukauden_numero = int(input('Anna kuukauden numero (1-12): '))
-if kuukauden_numero == 12 or 1 or 2:
-    print(vuodenajat[0])
-    kuukauden_numero = int(input('Anna kuukauden numero (1-12): '))
-elif kuukauden_numero == 3 or 4 or 5:
-    print(vuodenajat[1])
-    kuukauden_numero = int(input('Anna kuukauden numero (1-12): '))
-elif kuukauden_numero == 6 or 7 or 8:
-    print(vuodenajat[2])
-    kuukauden_numero = int(input('Anna kuukauden numero (1-12): '))
-elif kuukauden_numero == 9 or 10 or 11:
-    print(vuodenajat[3])
-    kuukauden_numero = int(input('Anna kuukauden numero (1-12): '))
+while True:
+    if kuukauden_numero == 12 or (kuukauden_numero == 1) or (kuukauden_numero == 2):
+        print(vuodenajat[0])
+        kuukauden_numero = int(input('Anna kuukauden numero (1-12): '))
+    elif kuukauden_numero == 3 or (kuukauden_numero == 4) or (kuukauden_numero == 5):
+        print(vuodenajat[1])
+        kuukauden_numero = int(input('Anna kuukauden numero (1-12): '))
+    elif kuukauden_numero == 6 or (kuukauden_numero == 7) or (kuukauden_numero == 8):
+        print(vuodenajat[2])
+        kuukauden_numero = int(input('Anna kuukauden numero (1-12): '))
+    elif kuukauden_numero == 9 or (kuukauden_numero == 10) or (kuukauden_numero == 11):
+        print(vuodenajat[3])
+        kuukauden_numero = int(input('Anna kuukauden numero (1-12): '))
