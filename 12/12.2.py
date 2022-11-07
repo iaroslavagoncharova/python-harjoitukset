3561579489e4ed8b910a1fdb9b335e6c07971cd8
darri <iaroslag@metropolia.fi>
2022-11-07 21:05:47 +0200

    tehtävät 12

--- 12/12.2.py
+++ 12/12.2.py
@@ -5,7 +5,10 @@ response = requests.get(url)
 data = response.json()
 lat = data[0]['lat']
 lon = data[0]['lon']
-url = f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&appid=c9dca395b68f0f13b3cb7f4eedc59865"
+url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=c9dca395b68f0f13b3cb7f4eedc59865"
 response = requests.get(url)
 data = response.json()
-print(data[0]['description'])
+print(f"The weather is {data['weather'][0]['description']}")
+kelvin = data['main']['temp']
+celsius = kelvin - 273.15
+print('The temperature is', round(celsius), 'degrees')
